5d91cad2ec15264ca273b74c7e039e21cbfbd887
zqh <407981121@qq.com>
2023-09-20 20:51:59 +0800

    练习

---
+++ src/main/java/com/springboot/gitspring/GitSpringApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.gitspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/gitspring/GitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.gitspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
