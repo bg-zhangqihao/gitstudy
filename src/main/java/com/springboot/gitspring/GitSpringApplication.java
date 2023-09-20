73491c79684d63770c8a5d4709f90a06c48eb396
zqh <407981121@qq.com>
2023-09-20 20:58:31 +0800

    第二次提交

--- src/main/java/com/springboot/gitspring/GitSpringApplication.java
+++ src/main/java/com/springboot/gitspring/GitSpringApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GitSpringApplication.class, args);
+        System.out.println("qiqiq");
+        //SpringApplication.run(GitSpringApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/springboot/gitspring/qq.java
@@ -0,0 +1,7 @@
+package com.springboot.gitspring;
+
+public class qq {
+    public static void main(String[] args) {
+        System.out.println("bbbb");
+    }
+}
